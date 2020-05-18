50bf343f573bbc49cc48679f236473f6635eb11a
Stivulya <hlukhanyk@gmail.com>
2020-05-18 11:01:29 +0300

    created project

---
+++ WpfSocialNetwork/WpfSocialNetwork/ChangePassword.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Service;
+using System.Windows;
+
+namespace WpfSocialNetwork
+{
+
+    public partial class ChangePassword : Window
+    {
+        UserService services;
+        string loginUsername;
+        public ChangePassword(string loginUsername)
+        {
+            InitializeComponent();
+            services = new UserService();
+            this.loginUsername = loginUsername;
+        }
+
+        private void Change(object sender, RoutedEventArgs e)
+        {
+            bool temp = services.UpdatePassword(loginUsername, oldPwd.Password, newPwd.Password);
+            if (temp == true)
+            {
+                MessageBox.Show("Done");
+            }
+            else
+            {
+                MessageBox.Show("Password was not changed");
+            }
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            General general = new General();
+            general.Show();
+            Close();
+        }
+    }
+}
---
+++ WpfSocialNetwork/WpfSocialNetwork/SearchUser.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Service;
+using System.Windows;
+
+namespace WpfSocialNetwork
+{
+    /// <summary>
+    /// Interaction logic for SearchUser.xaml
+    /// </summary>
+    public partial class SearchUser : Window
+    {
+        UserService service;
+        private string loginUsername;
+
+        public SearchUser(string username)
+        {
+            loginUsername = username;
+            InitializeComponent();
+            service = new UserService();
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+
+            if (service.CheckIfUserIsInDatabase(searchUser.Text))
+            {
+                Profile profile = new Profile(loginUsername, searchUser.Text)
+                {
+                    WindowStartupLocation = WindowStartupLocation.CenterScreen
+                };
+                profile.ShowDialog();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("There is no such user");
+            }
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            General general = new General();
+            general.Show();
+            Close();
+        }
+
+        private void searchUser_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
